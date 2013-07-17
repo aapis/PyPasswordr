66dbb91052b6d8bb2f33460dc99a8e22238d07e6
Ryan Priebe <rpriebe@me.com>
2013-07-17 00:35:48 -0600

    Initial commit.
    Python version of my Passwordr project, first python project everrrr.

---
+++ passwordr.py
@@ -0,0 +1,30 @@
+import sys
+
+#if __name__ == "__main__":
+class Passwordr(object):
+	"""Initialize the class"""
+	def __init__(self, arg):
+		super(Passwordr, self).__init__()
+		self.arg = arg
+
+		print self.arg
+
+	"""gets the ball rolling by taking a randomly generated string and running it through a hash generator (sha1 in this case)"""
+	def randomizer():
+		pass
+
+	"""takes confusitizer()'s array and adds some new random characters to it, depending on the length of the array"""
+	def rediscombobulator():
+		pass
+
+	"""chooses a random starting point for randomizer()'s string"""
+	def startmajigger():
+		pass
+
+	"""turns output from randomizer() and rediscombobulator() into a string"""
+	def confusitizer():
+		pass
+
+			
+# Instantiate the Passwordr class
+pw = Passwordr('test')
